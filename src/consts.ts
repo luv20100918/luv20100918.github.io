c16636eff15ccdc929f86ace71e0b6c9469e12f1
andew <croh@jejupass.co.kr>
2025-08-08 09:13:28 +0900

    Update blog title and remove sample posts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'AI시대에서 개발자가 사는 법';
+export const SITE_DESCRIPTION = 'AI 시대를 살아가는 개발자의 경험과 인사이트를 공유합니다.';
